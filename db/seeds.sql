29c55d1aabde2c5afe98ebd75a1df9b3a6228741
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-01-30 14:32:35 -0800

    changed name of workouts.js to workoutsData.js and deleted seeds.sql

--- db/seeds.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS workouts;
-
-CREATE TABLE workouts (
-    workout_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    stroke_id STRING NOT NULL,
-    duration TIME NOT NULL,
-    difficulty INTEGER NOT NULL
-);

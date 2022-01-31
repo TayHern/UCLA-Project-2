bcc68b772765b41f22c3170c1d49a878c4415378
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-01-30 16:42:34 -0800

    sessionData.js updated

--- seeds/sessionData.js
+++ seeds/sessionData.js
@@ -1,1 +1,12 @@
-//session data;
+//sessionData;
+const { Session } = require('../session');
+
+const sessionData = [
+    {
+    name: '',
+    description: '',
+    },
+];
+
+const sessions = () => Session.bulkCreate(sessionData);
+module.exports = sessions;

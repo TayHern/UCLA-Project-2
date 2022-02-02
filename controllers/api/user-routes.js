a30e39b6df0558d20871ce3ee475e24b11384fde
TAY-HERN <hernandeztaylor1@gmail.com>
2022-02-01 20:37:39 -0800

    add in user CRUD routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { User, Session, Sesswork, Workout } = require('../../models');
+
+
+//New User
+router.post('/', (req, res) => {
+  // expects {username: 'user1234', email: 'user@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Login user
+router.post('/login', (req, res) => {
+  // expects {email: 'user@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
+//Delete user
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
